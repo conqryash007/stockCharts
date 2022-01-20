e1d336ae687f3c6cd15f213663f9003af86880cb
conqryash007 <guptayash200010@gmail.com>
2022-01-20 22:28:21 +0530

    handle warnings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import ReactApexChart from "react-apexcharts";
 import Searchbar from "./Searchbar";
-import { MACD } from "technicalindicators";
+// import { MACD } from "technicalindicators";
 
 let seriesData = [{ x: "12 jan", y: [0, 0, 0, 0] }];
 let seriesBarData = [{ x: "12 jan", y: 100 }];
@@ -138,20 +138,21 @@ class ApexChart extends React.Component {
             y: demo,
           };
         });
+
         // data for histogram related to stock
 
-        let macdInput = {
-          values: [
-            127.75, 129.02, 132.75, 145.4, 148.98, 137.52, 147.38, 139.05,
-            137.23, 149.3, 162.45, 178.95, 200.35, 221.9, 243.23, 243.52,
-            286.42, 280.27,
-          ],
-          fastPeriod: 5,
-          slowPeriod: 8,
-          signalPeriod: 3,
-          SimpleMAOscillator: false,
-          SimpleMASignal: false,
-        };
+        // let macdInput = {
+        //   values: [
+        //     127.75, 129.02, 132.75, 145.4, 148.98, 137.52, 147.38, 139.05,
+        //     137.23, 149.3, 162.45, 178.95, 200.35, 221.9, 243.23, 243.52,
+        //     286.42, 280.27,
+        //   ],
+        //   fastPeriod: 5,
+        //   slowPeriod: 8,
+        //   signalPeriod: 3,
+        //   SimpleMAOscillator: false,
+        //   SimpleMASignal: false,
+        // };
         // console.log(macdInput, MACD.calculate(macdInput));
         // macdInput.values = values.map((curr) => {
         //   return curr["4. close"];
